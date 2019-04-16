a93b7fce61d8234a6b71a5a06ce4c35aab00a28c
Paul RASTOIN <prastoin@student.42.fr>
2019-04-16 15:47:18 +0200

    begining of sprite sheets management

--- script.js
+++ script.js
@@ -1,72 +1,101 @@
 "use strict"
 
 function between(x1, y1, x2, y2, width, height) {
-    if (x1 - 5 > x2 + width + 5 || x2 - 5 > x1 + width + 5 && y1 - 5 > y2 + height + 5 && y2 - 5 > y1 + height + 5)
+    if (x1 - 5 > x2 + width + 5 || x2 - 5 > x1 + width + 5)
+        return false;
+    if (y1 - 5 > y2 + height + 5 || y2 - 5 > y1 + height + 5)
         return false;
 	return true;
 }
 
-function _in(x1, y1, x2, y2, width, height) {
-    if (x1 - 5 > x2 + width + 5 && x2 - 5 > x1 + width + 5)
-        return false;
-    if (y1 - 5 > y2 + height + 5 && y2 - 5 > y1 + height + 5)
-        return false;
-	return true;
+//x1 y1 zone click
+function center(x1, y1, x2, y2, width, height) {
+	if ((x1 >= x2 && x1 <= x2 + width))
+		if ((y1 >= y2 && y1 <= y2 + height))
+			return true;
+		return false;
+}
+
+function ft_display_explosion(x, y, img, ctx, width, height)
+{
+	for(let i = 0; i < nb_frame; i++){
+		drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+	}
+}
+
+function explosion(x, y)
+{
+	const explo = document.getElementById('explosion');
+	canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+	const explo_ctx = explo_ctx.getContext('2d');
+
+	const explo_img = new Image();
+	explo_img.onload = function (){
+		ft_display_explosion(x, y, explo_img, explo_ctx, width, height);
+	}
+	explo_img = 'http://dgame-dev.de/templates/tutorial/0.6/images/explosion.png';
+}
+
+function ft_display(tab, ctx)
+{
+	tab.forEach(({x, y, file}) => ctx.drawImage(file, x, y, file.width, file.height));
 }
 
 function draw_prey() {
-	const canvas = document.getElementById('canvas')
+	const canvas = document.getElementById('canvas');
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
-    const ctx = canvas.getContext('2d')
+    const ctx = canvas.getContext('2d');
 
     //hunter spawn display
-    const hunter = new Image();
-    const img = new Image();
-    const tab_hunter = [];
-    const tab_prey = [];
+	const hunter = new Image();
+	const img = new Image();
 	const tab = [];
 	hunter.onload = function () {
-	img.onload = function () {
+		img.onload = function () {
+		img.height = hunter.height;
+		img.width = hunter.width;
 		for (let j = 0; j < 100; j++) {
 				let x;
 				let y;
 				
+				let guard = 0;
 				do {
-					x = Math.floor(Math.random() * (window.innerWidth - img.width - 10)) + 5;
-					y = Math.floor(Math.random() * (window.innerHeight - img.height - 10)) + 5;
-				} while(tab.some((coord) => (between(x, y, coord.x, coord.y, hunter.width, hunter.height))))
+					x = Math.floor(Math.random() * (window.innerWidth - hunter.width - 10)) + 5;
+					y = Math.floor(Math.random() * (window.innerHeight - hunter.height - 10)) + 5;
+					guard++;
+				} while(guard < 100 && tab.some((coord) => (between(x, y, coord.x, coord.y, hunter.width, hunter.height))))
+				if (guard == 100)
+					break ;
 				console.log(x, y);
-				tab.push({x, y});
-				(j % 2) == 0 ? ctx.drawImage(img, x, y, hunter.width, hunter.height) : ctx.drawImage(hunter, x, y, hunter.width, hunter.height);
-				(j % 2) == 0 ? tab_prey.push({ x, y }) : tab_hunter.push({x, y});
+				tab.push({x, y, type: (j % 2) ? 'hunter' : 'prey', file : (j % 2) ? hunter : img}); // possible dutiliser le res du terneaire juste avant ?
 			}
-			console.log(tab)
+		ft_display(tab, ctx);
 		};
+		if (img.complete)
+			img.onload();
 	}
 	hunter.src = 'https://mdn.mozillademos.org/files/5399/gallery_1.jpg';
 	img.src = 'https://mdn.mozillademos.org/files/5397/rhino.jpg';
-	continu(canvas, ctx, hunter, img, tab);
+	continu({canvas, ctx, hunter, img, tab});
 }
 
-function deal_click(event, canvas, ctx, hunter, prey, tab)
+function deal_click({event, canvas, ctx, hunter, prey, tab})
 {
-	let i = 0;
-	console.log(tab.length);
-	while (i < tab.length)
+	console.log(tab);
+	const clicked = tab.find(({ x, y }) => center(event.x, event.y, x, y, hunter.width, hunter.height));
+	if (clicked)
 	{
-		if (!_in(event.x, event.y, tab[i].x, tab[i].y, hunter.width, hunter.height))
-			console.log("JE CLIQUE BIEN SUR QQLS CHOSE");
-		else
-			console.log("NOTHING");
-		i++;
+		console.log(clicked.type);
+		tab.splice(tab.indexOf(clicked), 1);
+		ctx.clearRect(clicked.x, clicked.y, clicked.file.width, clicked.file.height);
 	}
-	console.log("coucou");
 }
 
-function continu(canvas, ctx, hunter, prey, tab) {
+function continu({canvas, ...data}) {
 	canvas.addEventListener('click', (event) => {
-		deal_click(event, canvas, ctx, hunter, prey, tab)
+		deal_click({canvas, ...data, event});
 	}, false);
 };
 
