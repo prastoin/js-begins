1177689416fda147dc5e1e85e960fd07fca122d9
Paul RASTOIN <prastoin@student.42.fr>
2019-05-06 07:43:16 +0200

    new things

--- script.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-function draw_sprite(img, i, j, x, y, width, height, explo_ctx, t_width, t_height)
-{
-	console.log("ca sprite severe !");
-	if (i != 0 && j != 0)
-		explo_ctx.clearRect(x, y, t_width, t_height);
-	explo_ctx.drawImage(img, width + (i * width), height + (j * height), width, height, x, y, t_width, t_height);
-}
